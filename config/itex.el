a73a15728ae780a51af1a0f4176277612223d5fd
nameiwillforget <ahprae@protonmail.com>
2023-06-07 11:18:38 +0200

    Update

---
+++ config/itex.el
@@ -0,0 +1,183 @@
+(setq cdlatex-math-symbol-alist
+      '(
+	(?p ("\\pentagon"))
+
+	(?u ("\\nwarrow"))
+
+	(?ü ("\\big" "\\Big" "\\bigg" "\\Bigg"))
+	(?[ ("\\left["))
+
+	(?, ("\\nearrow"))
+	(?] ("\\right]"))
+
+	(?q ("\\dag"))
+	(?^ ("\\dagger"))
+
+
+	(?? ("\\fcmp"))
+
+
+	(?< ("\\leq"))
+
+	(?l ("\\ell"))
+
+
+	(?m ("\\diamond"))
+	(?= ("\\cong"))
+
+
+	(?& ("\\&"))
+
+
+
+	(?i ("\\longleftarrow"))
+	(?/ ("\\setminus"))
+
+
+	(?{ ("\\{"))
+
+	(?a ("\\longrightarrow"))
+	(?{ ("\\}"))
+
+
+	(?* ("\\cdot" "\\star"))
+
+	(?d ("\\div"))
+
+	(?o ("\\models"))
+
+
+	(?t ("\\left"))
+	(?r ("\\right"))
+
+	(?n ("\\dashv"))
+	(?- ("\\ominus"))
+
+	(?- ("\\leftharpoonup" "\\leftharpoondown"))
+
+	(?s ("\\vdash"))
+	(?: ("\\rightharpoonup" "\\rightharpoondown"))
+
+	(?ß ("\\bigcirc"))
+
+	(?k ("\\swarrow"))
+
+
+	(?. ("\\searrow"))
+	(?~ ("\\simeq"))
+
+	(?j ("\\oplus"))
+	(?+ ("\\pm" "\mp"))
+
+	(?g ("\\langle"))
+
+	(?z ("\\rangle"))
+
+	(?w ("\\angle"))
+	(?' ("\\prime"))
+
+	(?b ("\\not"))
+
+
+	
+	(?α ("\\alpha"))
+	(?β ("\\beta"))
+	(?γ ("\\gamma"))
+	(?δ ("\\delta"))
+	(?ε ("\\epsilon"))
+	(?ζ ("\\zeta"))
+	(?η ("\\eta"))
+	(?θ ("\\theta"))
+	(?ι ("\\iota"))
+	(?κ ("\\kappa"))
+	(?λ ("\\lambda"))
+	(?μ ("\\mu"))
+	(?ν ("\\nu"))
+	(?ξ ("\\xi"))
+	(?π ("\\pi"))
+	(?σ ("\\sigma"))
+	(?ς ("\\varsigma"))
+	(?τ ("\\tau"))
+	(?υ ("\\upsilon"))
+	(?φ ("\\phi"))
+	(?χ ("\\chi"))
+	(?ψ ("\\psi"))
+	(?ω ("\\omega"))
+
+	(?Γ ("\\Gamma"))
+	(?Δ ("\\Delta"))
+	(?Θ ("\\Theta"))
+	(?Λ ("\\Lambda"))
+	(?Ξ ("\\Xi"))
+	(?Π ("\\Pi"))
+	(?Σ ("\\Sigma"))
+	(?Φ ("\\Phi"))
+	(?Ψ ("\\Psi"))
+	(?Ω ("\\Omega"))
+
+	(?ℵ ("\\aleph"))
+	(?ב ("\\beth"))
+	
+
+	(?ℕ ("\\mathbb\{N\}"))
+	(?ℤ ("\\mathbb\{Z\}"))
+	(?ℚ ("\\mathbb\{Q\}"))
+	(?ℝ ("\\mathbb\{R\}"))
+	(?ℂ ("\\mathbb\{C\}"))
+
+	(?ℍ ("\\mathbb\{H\}"))
+	(?𝕂 ("\\mathbb\{K\}"))
+	(?𝕄 ("\\mathbb\{M\}"))
+	(?𝕋 ("\\mathbb\{T\}"))
+	(?𝕍 ("\\mathbb\{V\}"))
+	(?𝕐 ("\\mathbb\{Y\}"))
+
+
+
+	
+	(?χ ("\\Uparrow" "\\Uuparrow"))
+	(?η ("\\Leftarrow" "\\Lleftarrow"))
+	(?ε ("\\Downarrow" "\\Ddownarrow"))
+	(?α ("\\Rightarrow" "\\Rrightarrow"))
+
+
+
+
+	(?∘ ("\\odot" "\\astrosun"))
+
+
+
+
+	(?λ ("\\cup" "\\bigcup" "\\medtriangleup"))
+	(?τ("\\supset" "\\supseteq" "\\medtriangleright"))
+	(?ρ("\\cap" "\\bigcap" "\\medtriangledown"))
+	(?ν("\\subset" "\\subseteq" "\\medtriangleleft"))
+
+
+
+
+
+
+	(?× ("\\otimes" "\\boxtimes"))
+
+
+
+
+	(?⋉ ("\\lftimes" "\\lfbowtie"))
+	(?⋊ ("\\rftimes" "\\rfbowtie"))
+	(?≺ ("\\preqeq" "\\pereqsim"))
+
+	(?≻ ("\\succeq" "\\succsim"))
+
+
+	(?>("\\geq" "\\gg" "\\ggg"))
+
+	(?<("\\leq" "\\ll" "\\lll"))
+
+
+
+	(?← ("\\hookleftarrow" "\\twoheadleftarrow" "\\leftleftarrows"))
+	(?↑ ("\\hookuparrow" "\\twoheaduparrow" "\\upuparrows"))
+	(?→ ("\\hookrightarrow" "\\twoheadrightarrow" "\\rightrightarrows"))
+	(?↓ ("\\hookuparrow" "\\twoheaduparrow" "\\downdownarrows"))
+    ))
